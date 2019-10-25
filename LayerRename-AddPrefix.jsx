f04a1f2918e985072c155567de9ce0042b972c8a
Rizal Renaldi <rizalrenaldi@gmail.com>
2019-10-26 04:22:19 +0700

    first commit

--- LayerRename-AddPrefix.jsx
+++ LayerRename-AddPrefix.jsx
@@ -1,6 +1,6 @@
 var input = prompt("Enter Prefix", "");
-
 var docRef = app.activeDocument;
+var cancelBtn = asker.add('button',undefined,'Cancel');
 
   for(i=0;docRef.layers.length>i;i++){
    var currentLayer = docRef.layers[i];
@@ -10,3 +10,7 @@ var docRef = app.activeDocument;
   }
 
 alert("Layers renamed");
+
+cancelBtn.onClick=function(){
+  this.parent.close();  
+ }
