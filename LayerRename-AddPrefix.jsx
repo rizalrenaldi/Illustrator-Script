478a7b1f81f511e750ae2ecc6bc41c893089569a
Rizal Renaldi <rizalrenaldi@gmail.com>
2019-10-26 03:58:32 +0700

    first commit

--- LayerRename-AddPrefix.jsx
+++ LayerRename-AddPrefix.jsx
@@ -2,13 +2,11 @@ var input = prompt("Enter Layer Name", "");
 
 var docRef = app.activeDocument;
 
-     for(i=0;docRef.layers.length>i;i++){
-       var currentLayer = docRef.layers[i];
-          if (currentLayer.visible) {
-            currentLayer.name = input + currentLayer.name;
-          }
-     }
-
-
+  for(i=0;docRef.layers.length>i;i++){
+   var currentLayer = docRef.layers[i];
+      if (currentLayer.visible) {
+        currentLayer.name = input + currentLayer.name;
+      }
+  }
 
 alert("Layers renamed");
